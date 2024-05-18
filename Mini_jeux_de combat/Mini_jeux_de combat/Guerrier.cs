e07a140d0bbf6d744088c70568c4f04091c57e2d
RAKOTOARIMANANA Tojo Ny Aina <132362055+ItsTojoNyAina@users.noreply.github.com>
2024-05-18 11:18:44 +0300

    Add files via upload

---
+++ Mini_jeux_de combat/Mini_jeux_de combat/Guerrier.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Mini_jeux_de_combat
+{
+	public class Guerrier
+	{
+		private string Nom { get; set; }
+		private int Attaque { get; set; }
+		private int Defense { get; set; }
+		private int Sante { get; set; }
+		private int Mana { get; set; }
+		public Guerrier(string nom,int attaque, int defense, int sante,int mana)
+		{
+			this.Nom = nom;
+			this.Attaque = attaque;
+			this.Defense = defense;
+			this.Sante = sante;
+			this.Mana = 100;
+		}
+		public Guerrier() { }
+		public void AfficherInformation() {
+			Console.WriteLine("Nom:" + this.Nom);
+            Console.Write("Attaque:" + this.Attaque);
+            Console.Write("Défense:" + this.Defense);
+            Console.Write("Santé:" + this.Sante);
+        }
+		public void LancerAttaqueSur(Guerrier cible) {
+			cible.Sante -= Attaque;
+			AfficherInformation();
+			cible.AfficherInformation();
+		}
+		public void LancerAttqAvcDef (Guerrier cible){
+			Console.WriteLine($"{Nom} essaie d'attaquer"+cible.Nom);
+			if (Attaque > cible.Defense) {
+				LancerAttaqueSur(cible);
+			}
+			else {
+				Console.WriteLine("Vous êtes trop faible pour attaquer le Goat en face de vous......Maintenant Fuck You");
+				Sante -= (cible.Defense - Attaque); 
+			}
+            AfficherInformation();
+            cible.AfficherInformation();
+        }
+		public void ActiverSoin() {
+			if (Mana >= 10) {
+				Console.WriteLine($"{Nom} essaie de se soigner!");
+				Mana -= 10;
+				Sante += 10;
+				Console.WriteLine($"Soin réussi nouvelle stat de Santé: {Sante}");
+            }
+			else {
+				Console.WriteLine("Pas assez de mana pour se soigner");
+			}
+		}
+		
+	}
+}
+
---
+++ Mini_jeux_de combat/Mini_jeux_de combat/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Mini_jeux_de_combat
+{
+    class Program {
+        static void Main(string[] args) {
+            Guerrier g1 = new Guerrier("Tojo", 400,200,300,120);
+            Guerrier g2 = new Guerrier("Botin", 200, 100, 400, 130);
+            g2.LancerAttqAvcDef(g1);
+            g1.LancerAttqAvcDef(g2);
+            g2.ActiverSoin();
+            g1.ActiverSoin();
+        }
+    }
+}

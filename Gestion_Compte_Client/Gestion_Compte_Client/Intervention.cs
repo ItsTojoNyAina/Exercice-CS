009382084a0b640e5b089839dc9c467dfa2649df
RAKOTOARIMANANA Tojo Ny Aina <132362055+ItsTojoNyAina@users.noreply.github.com>
2024-08-17 15:44:54 +0300

    Add files via upload

---
+++ Gestion_Compte_Client/Gestion_Compte_Client/Intervention.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Compte_Client
+{
+    internal class Intervention
+    {
+        public List<Intervenant> Intervenants { get; set; }
+
+            public Intervention(List<Intervenant> intervenants)
+            {
+                Intervenants = intervenants;
+            }
+
+            public double AjustementQualificationEuler(int nombreIterations, double pas, double seuil)
+            {
+                double coutTotal = 0;
+
+                foreach (var intervenant in Intervenants)
+                {
+                    double deltaQualification = pas;
+
+                    for (int i = 0; i < nombreIterations; i++)
+                    {
+                        intervenant.AjusterQualification(deltaQualification);
+                        double coutIntervenant = intervenant.CalculerCout();
+                        coutTotal += coutIntervenant;
+
+                        // Option : arrêter si la variation est trop faible
+                        if (Math.Abs(deltaQualification * intervenant.QualificationIntervention) < seuil)
+                            break;
+                    }
+                }
+
+                return coutTotal;
+            }
+        }
+    }
+
---
+++ Gestion_Compte_Client/Gestion_Compte_Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gestion_Compte_Client
+{ 
+    class Program
+    {
+        static void Main()
+        {
+            // Exemples d'intervenants
+            var intervenant1 = new Intervenant("Alice", "Qualification 1", 5.0, 300.0, 10);
+            var intervenant2 = new Intervenant("Bob", "Qualification 2", 4.0, 400.0, 8);
+            var intervenant3 = new Intervenant("Charlie", "Qualification 3", 6.0, 500.0, 12);
+
+            // Liste d'intervenants pour l'intervention
+            var intervenants = new List<Intervenant> { intervenant1, intervenant2, intervenant3 };
+
+            // Création de l'intervention
+            var intervention = new Intervention(intervenants);
+
+            // Ajustement des qualifications avec l'algorithme d'Euler
+            double coutTotalFinal = intervention.AjustementQualificationEuler(nombreIterations: 100, pas: 0.01, seuil: 0.001);
+
+            // Affichage du coût total final
+            Console.WriteLine("Le coût total final de l'intervention est : " + coutTotalFinal);
+        }
+    }
+}
